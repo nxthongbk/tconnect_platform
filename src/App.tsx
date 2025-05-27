29d1dde1caf61a603f7e496f916c7154cd59adee
quockhanhnguyentruong <nhokbjn1997@gmail.com>
2025-05-27 21:36:06 +0700

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import { Suspense, useContext } from 'react';
+import { Suspense } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import './App.scss';
 import LoadingSuspense from './components/LoadingSuspense';
 import useRoutes from './config/routes';
-import { AppContext } from './contexts/app.context';
 
 function App() {
-  const { userInfo } = useContext(AppContext);
   return (
     <Suspense fallback={<LoadingSuspense />}>
-      <RouterProvider router={useRoutes(userInfo?.roles[0])} />
+      <RouterProvider router={useRoutes()} />
     </Suspense>
   );
 }
