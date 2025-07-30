98b2009a455959bfa22a205432c7eb27f23235e5
Thong <nxthongbk@gmail.com>
2025-07-30 09:28:52 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -8,14 +8,14 @@ export default defineConfig({
   server: {
     port: 3000,
     host: true,
-    // allowedHosts: ['esg.innovation.com.vn'],
-    allowedHosts: ['*'],
+    allowedHosts: ['tmes.innovation.com.vn'],
+    //allowedHosts: ['*'],
   },
   preview: {
     host: true,
     port: 3000,
-    // allowedHosts: ['emp.innovation.com.vn'],
-    allowedHosts: ['*'],
+    allowedHosts: ['tmes.innovation.com.vn'],
+    //allowedHosts: ['*'],
   },
   css: {
     devSourcemap: true,
